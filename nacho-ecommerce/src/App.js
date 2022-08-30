830f9c1d3db5c793e130099e59eeab0f7f7d3d3f
Ignacio Iantorno <ignacioiantorno@gmail.com>
2022-08-30 00:53:59 -0300

    first commit

---
+++ nacho-ecommerce/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./styles.js";
+
+import { styles } from "./styles";
+
+// INTERPOLAR VARIABLES DE JS
+// JSS
+
+// COMPONENTS
+import NavBar from "./components/NavBar";
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
